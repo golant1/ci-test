8e62d4d9a0f57ba50c53cdfe7055a62ad36e830f
Oleg Grigorov <golantster@gmail.com>
2017-07-25 19:29:45 +0300

    update

--- devops-pipeline.groovy
+++ devops-pipeline.groovy
@@ -1,4 +1,5 @@
-common = new com.mirantis.mk.Common()
+
+n = new com.mirantis.mk.Common()
 
 /**
  * Creates env according to input params by DevOps tool
@@ -36,8 +37,35 @@ def startupDevOpsEnv(path, env){
     """, returnStdout: true)
 }
 
-node {
-    stage ('creating environmet') {
+/**
+ * Get env IP 
+ * 
+ * @param path Path to dos.py 
+ * @param env name of the ENV to find out IP 
+  */
+def getDevOpsIP(path, env){
+    return sh(script:"""
+    ${path} slave-ip-list --address-pool-name public-pool01 --ip-only ${env}
+    """, returnStdout: true)
+}
+
+def ifEnvIsReady(envip){
+    def retries = 50
+    if (retries != -1){
+        retry(retries){
+            return sh(script:"""
+            nc -z -w 30 ${envip} 22
+            """, returnStdout: true)
+        }
+    } else {
+        echo "It seems the env has not been started properly"
+    }    
+}
+
+
+node ('tpi77'){
+    devops_path = '/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py'
+    stage ('Creating environmet') {
         if ("${params.ENV_NAME}" == '') {
             error("ENV_NAME have to be defined")
         }
@@ -49,16 +77,30 @@ node {
             echo "Multi"
         }
         try {
-            createDevOpsEnv('/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py',"${tpl}")
+            createDevOpsEnv("${devops_path}","${tpl}")
         } catch (err) {
             error("${err}")
 //            eraseDevOpsEnv("${params.ENV_NAME}")   
         }
+    }
+     stage ('Bringing up the environment') {
         try {
-            startupDevOpsEnv('/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py',"${params.ENV_NAME}")
+            startupDevOpsEnv("${devops_path}","${params.ENV_NAME}")
         } catch (err) {
             error("${params.ENV_NAME} has not been managed to bring up")
-        }        
+        }
+     }
+     stage ('Getting environment IP') {
+        try {
+            envip = getDevOpsIP("${devops_path}","${params.ENV_NAME}").trim()
+            echo "${envip}"
+        } catch (err) {
+            error("IP of the env ${params.ENV_NAME} can't be got")
+        }                
     }
+     stage ('Checking whether the env has finished starting') {
+         ifEnvIsReady("${envip}")
+     }
+    
 }
 
