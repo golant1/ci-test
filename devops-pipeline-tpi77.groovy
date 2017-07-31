977e8d11520d0927c08f105728786b1d37a91a9a
Oleg Grigorov <golantster@gmail.com>
2017-07-31 13:41:41 +0300

    some changes

--- devops-pipeline-tpi77.groovy
+++ devops-pipeline-tpi77.groovy
@@ -1,16 +1,28 @@
+common = new com.mirantis.mk.Common()
 
-n = new com.mirantis.mk.Common()
+/**
+* ENV_NAME              The prefix for env name is going to be created
+* TEMPLATE              There are two templates are available for one-node installation and two-node (Single or Multi)
+* DESTROY_ENV           To shutdown env once job is finished
+* DEPLOY_OPENSTACK      if set True OpenStack will be deployed
+*
+*/
+
 
 /**
  * Creates env according to input params by DevOps tool
  * 
  * @param path Path to dos.py 
+ * @param work_dir path where devops is installed
  * @param type Path to template having been created
  */
-def createDevOpsEnv(path, tpl){
-    echo "${path} ${tpl}"
+def createDevOpsEnv(path, work_dir, tpl, envname){
+//    echo "${path} ${tpl}"
     return sh(script:"""
-    export ENV_NAME=${params.ENV_NAME} &&
+    export ENV_NAME=${envname} &&
+    export WORKING_DIR=${work_dir} &&
+    export DEVOPS_DB_NAME=${work_dir}/fuel-devops.sqlite &&
+    export DEVOPS_DB_ENGINE=django.db.backends.sqlite3 && 
     ${path} create-env ${tpl}
     """, returnStdout: true)
 }
@@ -23,16 +35,37 @@ def createDevOpsEnv(path, tpl){
   */
 def eraseDevOpsEnv(path, env){
     echo "${env} will be erased"
+    return sh(script:"""
+    ${path} erase ${env}
+    """, returnStdout: true)
+}
+
+/**
+ * Shutdown the env 
+ *
+ * @param path Path to dos.py  
+ * @param env name of the ENV have to be deleted 
+  */
+def destroyDevOpsEnv(path, env){
+    echo "${env} will be erased"
+    return sh(script:"""
+    ${path} destroy ${env}
+    """, returnStdout: true)    
 }
 
 /**
  * Starts the env 
  * 
  * @param path Path to dos.py 
+ * @param work_dir path where devops is installed
  * @param env name of the ENV have to be brought up 
   */
-def startupDevOpsEnv(path, env){
+def startupDevOpsEnv(path, work_dir, env){
     return sh(script:"""
+    export ENV_NAME=${params.ENV_NAME} &&
+    export WORKING_DIR=${work_dir} &&
+    export DEVOPS_DB_NAME=${work_dir}/fuel-devops.sqlite &&
+    export DEVOPS_DB_ENGINE=django.db.backends.sqlite3 && 
     ${path} start ${env}
     """, returnStdout: true)
 }
@@ -41,10 +74,15 @@ def startupDevOpsEnv(path, env){
  * Get env IP 
  * 
  * @param path Path to dos.py 
+ * @param work_dir path where devops is installed
  * @param env name of the ENV to find out IP 
   */
-def getDevOpsIP(path, env){
+def getDevOpsIP(path, work_dir, env){
     return sh(script:"""
+    export ENV_NAME=${params.ENV_NAME} &&
+    export WORKING_DIR=${work_dir} &&
+    export DEVOPS_DB_NAME=${work_dir}/fuel-devops.sqlite &&
+    export DEVOPS_DB_ENGINE=django.db.backends.sqlite3 && 
     ${path} slave-ip-list --address-pool-name public-pool01 --ip-only ${env}
     """, returnStdout: true)
 }
@@ -57,14 +95,19 @@ def ifEnvIsReady(envip){
             nc -z -w 30 ${envip} 22
             """, returnStdout: true)
         }
+        common.successMsg("The env with IP ${envip} has been started")
     } else {
         echo "It seems the env has not been started properly"
     }    
 }
 
 
-node ('tpi77'){
-    devops_path = '/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py'
+node('tpi77') {
+    devops_dos_path = '/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py'
+    devops_work_dir = '/var/fuel-devops-venv'
+    def dt = new Date().getTime()
+    def envname = "${params.ENV_NAME}-${dt}"
+    
     stage ('Creating environmet') {
         if ("${params.ENV_NAME}" == '') {
             error("ENV_NAME have to be defined")
@@ -77,30 +120,53 @@ node ('tpi77'){
             echo "Multi"
         }
         try {
-            createDevOpsEnv("${devops_path}","${tpl}")
+            createDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${tpl}","${envname}")
         } catch (err) {
             error("${err}")
 //            eraseDevOpsEnv("${params.ENV_NAME}")   
         }
     }
-     stage ('Bringing up the environment') {
-        try {
-            startupDevOpsEnv("${devops_path}","${params.ENV_NAME}")
-        } catch (err) {
-            error("${params.ENV_NAME} has not been managed to bring up")
-        }
-     }
-     stage ('Getting environment IP') {
-        try {
-            envip = getDevOpsIP("${devops_path}","${params.ENV_NAME}").trim()
-            echo "${envip}"
-        } catch (err) {
-            error("IP of the env ${params.ENV_NAME} can't be got")
-        }                
+    stage ('Bringing up the environment') {
+       try {
+           startupDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${envname}")
+       } catch (err) {
+           error("${params.ENV_NAME} has not been managed to bring up")
+       }
     }
-     stage ('Checking whether the env has finished starting') {
-         ifEnvIsReady("${envip}")
-     }
+    stage ('Getting environment IP') {
+       try {
+           envip = getDevOpsIP("${devops_dos_path}","${devops_work_dir}","${params.ENV_NAME}").trim()
+           echo "${envip}"
+       } catch (err) {
+           error("IP of the env ${envname} can't be got")
+       }                
+    }
+    stage ('Checking whether the env has finished starting') {
+        ifEnvIsReady("${envip}")
+        if (DEPLOY_OPENSTACK.toBoolean() == true) {
+            stage ('Deploying Openstack') {
+                build(job: "deploy-heat-virtual_mcp11_aio", parameters: [
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_IMAGE', value: "sandriichenko/rally_tempest_docker:docker_aio"],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_PATTERN', value: "set=smoke"],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_TARGET', value: "I@salt:master"],
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_CREDENTIALS', value: "salt-qa-credentials"],
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_URL', value: "http://${envip}:6969"],
+                    [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: "openstack"],
+                    [$class: 'StringParameterValue', name: 'STACK_TEST', value: "openstack"],                 
+                    [$class: 'StringParameterValue', name: 'STACK_TYPE', value: "physical"]
+                ])
+            }
+        }
+        if (DESTROY_ENV.toBoolean() == true) {
+             stage ('Bringing down ${envname} environmnet') {
+                 try {
+                     destroyDevOpsEnv("${devops_path}","${envname}")
+                 } catch (err) {
+                     error("${envname} has not been managed to bring down")
+                 }
+             }
+         }
+    }
     
 }
 
--- devops-pipeline.groovy
+++ devops-pipeline.groovy
@@ -1,4 +1,13 @@
-n = new com.mirantis.mk.Common()
+common = new com.mirantis.mk.Common()
+
+/**
+* ENV_NAME              The prefix for env name is going to be created
+* TEMPLATE              There are two templates are available for one-node installation and two-node (Single or Multi)
+* DESTROY_ENV           To shutdown env once job is finished
+* DEPLOY_OPENSTACK      if set True OpenStack will be deployed
+*
+*/
+
 
 /**
  * Creates env according to input params by DevOps tool
@@ -7,10 +16,10 @@ n = new com.mirantis.mk.Common()
  * @param work_dir path where devops is installed
  * @param type Path to template having been created
  */
-def createDevOpsEnv(path, work_dir, tpl){
-    echo "${path} ${tpl}"
+def createDevOpsEnv(path, work_dir, tpl, envname){
+//    echo "${path} ${tpl}"
     return sh(script:"""
-    export ENV_NAME=${params.ENV_NAME} &&
+    export ENV_NAME=${envname} &&
     export WORKING_DIR=${work_dir} &&
     export DEVOPS_DB_NAME=${work_dir}/fuel-devops.sqlite &&
     export DEVOPS_DB_ENGINE=django.db.backends.sqlite3 && 
@@ -26,6 +35,22 @@ def createDevOpsEnv(path, work_dir, tpl){
   */
 def eraseDevOpsEnv(path, env){
     echo "${env} will be erased"
+    return sh(script:"""
+    ${path} erase ${env}
+    """, returnStdout: true)
+}
+
+/**
+ * Shutdown the env 
+ *
+ * @param path Path to dos.py  
+ * @param env name of the ENV have to be deleted 
+  */
+def destroyDevOpsEnv(path, env){
+    echo "${env} will be erased"
+    return sh(script:"""
+    ${path} destroy ${env}
+    """, returnStdout: true)    
 }
 
 /**
@@ -80,6 +105,9 @@ def ifEnvIsReady(envip){
 node {
     devops_dos_path = '/var/fuel-devops-venv/fuel-devops-venv/bin/dos.py'
     devops_work_dir = '/var/fuel-devops-venv'
+    def dt = new Date().getTime()
+    def envname = "${params.ENV_NAME}-${dt}"
+    
     stage ('Creating environmet') {
         if ("${params.ENV_NAME}" == '') {
             error("ENV_NAME have to be defined")
@@ -92,7 +120,7 @@ node {
             echo "Multi"
         }
         try {
-            createDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${tpl}")
+            createDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${tpl}","${envname}")
         } catch (err) {
             error("${err}")
 //            eraseDevOpsEnv("${params.ENV_NAME}")   
@@ -100,7 +128,7 @@ node {
     }
     stage ('Bringing up the environment') {
        try {
-           startupDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${params.ENV_NAME}")
+           startupDevOpsEnv("${devops_dos_path}","${devops_work_dir}","${envname}")
        } catch (err) {
            error("${params.ENV_NAME} has not been managed to bring up")
        }
@@ -110,23 +138,34 @@ node {
            envip = getDevOpsIP("${devops_dos_path}","${devops_work_dir}","${params.ENV_NAME}").trim()
            echo "${envip}"
        } catch (err) {
-           error("IP of the env ${params.ENV_NAME} can't be got")
+           error("IP of the env ${envname} can't be got")
        }                
     }
     stage ('Checking whether the env has finished starting') {
         ifEnvIsReady("${envip}")
-        stage ('Deploying Openstack') {
-            build(job: "deploy-heat-virtual_mcp11_aio", parameters: [
-                [$class: 'StringParameterValue', name: 'TEST_TEMPEST_IMAGE', value: "sandriichenko/rally_tempest_docker:docker_aio"],
-                [$class: 'StringParameterValue', name: 'TEST_TEMPEST_PATTERN', value: "set=smoke"],
-                [$class: 'StringParameterValue', name: 'TEST_TEMPEST_TARGET', value: "I@salt:master"],
-                [$class: 'StringParameterValue', name: 'SALT_MASTER_CREDENTIALS', value: "salt-qa-credentials"],
-                [$class: 'StringParameterValue', name: 'SALT_MASTER_URL', value: "http://${envip}:6969"],
-                [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: "openstack"],
-                [$class: 'StringParameterValue', name: 'STACK_TEST', value: "openstack"],                 
-                [$class: 'StringParameterValue', name: 'STACK_TYPE', value: "physical"]
-           ])
+        if (DEPLOY_OPENSTACK.toBoolean() == true) {
+            stage ('Deploying Openstack') {
+                build(job: "deploy-heat-virtual_mcp11_aio", parameters: [
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_IMAGE', value: "sandriichenko/rally_tempest_docker:docker_aio"],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_PATTERN', value: "set=smoke"],
+                    [$class: 'StringParameterValue', name: 'TEST_TEMPEST_TARGET', value: "I@salt:master"],
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_CREDENTIALS', value: "salt-qa-credentials"],
+                    [$class: 'StringParameterValue', name: 'SALT_MASTER_URL', value: "http://${envip}:6969"],
+                    [$class: 'StringParameterValue', name: 'STACK_INSTALL', value: "openstack"],
+                    [$class: 'StringParameterValue', name: 'STACK_TEST', value: "openstack"],                 
+                    [$class: 'StringParameterValue', name: 'STACK_TYPE', value: "physical"]
+                ])
+            }
         }
+        if (DESTROY_ENV.toBoolean() == true) {
+             stage ('Bringing down ${envname} environmnet') {
+                 try {
+                     destroyDevOpsEnv("${devops_path}","${envname}")
+                 } catch (err) {
+                     error("${envname} has not been managed to bring down")
+                 }
+             }
+         }
     }
     
 }
