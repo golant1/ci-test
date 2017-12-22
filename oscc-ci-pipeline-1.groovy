d1873456b61134a3c706fb27ca42165573971876
Oleg Grigorov <golantster@gmail.com>
2017-12-22 12:47:42 +0200

    bugfix

--- oscc-ci-pipeline-1.groovy
+++ oscc-ci-pipeline-1.groovy
@@ -114,7 +114,7 @@ def snapshotPublish(server, snapshot, distribution, components, prefix) {
 
 //    String data = "{\"SourceKind\": \"snapshot\", \"Sources\": [{\"Name\": \"${snapshot}\", \"Component\": \"${components}\" }], \"Architectures\": [\"amd64\"], \"Distribution\": \"${distribution}\"}"
 
-    aptly.promotePublish(server, 'xenial/nightly', "${prefix}/${distribution}", 'false', components, '', '', '-d --timeout 1200', '')
+    aptly.promotePublish(server, 'xenial/nightly', "${prefix}/${distribution}", 'false', components, '', '', '-d --timeout 1200', '', '')
 
 //    return restPost(server, "/api/publish/${prefix}", data)
 
