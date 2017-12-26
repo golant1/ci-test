cda598b60c9c500205987bfecbba2f6711edcb9c
Oleg Grigorov <golantster@gmail.com>
2017-12-26 14:40:40 +0200

    update

--- oscc-ci-pipeline-1.groovy
+++ oscc-ci-pipeline-1.groovy
@@ -151,7 +151,7 @@ def snapshotCreate(server, repo, package_refs = null) {
     def snapshot = "${repo}-${ts}-oscc-dev"
 
     if (package_refs) {
-        String listString = package_refs.join("\"],[\"")
+        String listString = package_refs.join("\",\"")
         println ("LISTSTRING: ${listString}")
         String data = "{\"Name\":\"${snapshot}\", \"Description\": \"OpenStack Core Components salt formulas CI\", \"PackageRefs\": [\"${listString}\"]}"
         echo "data: ${data}"
