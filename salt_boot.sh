6163600dbbdefda8e84bf12ee94cfa47f4fd7512
Oleg Grigorov <golantster@gmail.com>
2018-02-01 16:51:48 +0200

    add script

---
+++ salt_boot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Map hostnames of nodes and how it will be named for mcp
+declare -A hostnames
+hostnames=(["cz7935-kvm"]="kvm01.sriov-neutron.local" ["cz7936-kvm"]="kvm02.sriov-neutron.local" ["cz7937-kvm"]="cmp002.sriov-neutron.local" ["cz7903-kvm"]="gtw01.sriov-neutron.local" ["cz7904-kvm"]="cmp001.sriov-neutron.local")
+
+# Install salt-minion
+apt-get install --yes salt-minion
+HOSTNAME=$(hostname)
+
+# Apply initial minion.conf
+echo "id: ${hostnames[${HOSTNAME}]}" >>  /etc/salt/minion.d/minion.conf
+echo "master: 10.109.9.196" >> /etc/salt/minion.d/minion.conf
+
+# Restart salt-minion
+service salt-minion stop && service salt-minion start
+
