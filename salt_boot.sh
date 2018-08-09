b225ce498e672f469e48530f6900936b0e11cd16
Oleg Grigorov <golantster@gmail.com>
2018-08-09 13:01:30 +0300

    update

--- salt_boot.sh
+++ salt_boot.sh
@@ -4,6 +4,10 @@
 declare -A hostnames
 hostnames=(["cz7935-kvm"]="kvm01.sriov-neutron.local" ["cz7936-kvm"]="kvm02.sriov-neutron.local" ["cz7937-kvm"]="kvm03.sriov-neutron.local" ["cz7903-kvm"]="cmp002.sriov-neutron.local" ["cz7904-kvm"]="cmp001.sriov-neutron.local")
 
+echo "deb [arch=amd64] http://mirror.mirantis.com/proposed/saltstack-2016.3/xenial xenial main" >> /etc/apt/sources.list.d/mcp_saltstack.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E08A149DE57BFBE
+apt update
+
 # Install salt-minion
 apt-get install --yes salt-minion
 HOSTNAME=$(hostname)
